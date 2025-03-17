ced0962fa4f1ed5d0bb3589f63cfad1a890d5d2f
ChooJunIan <ianchoo2000@gmail.com>
2025-03-17 18:52:13 +0800

    Complete reset button of the drawing board

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-let container = document.querySelector('.container');
+function createBoard() {
+  let container = document.querySelector('.container');
 
-for (let row = 0; row < 16; row++) {
-  let newDivRow = document.createElement('div');
-  newDivRow.setAttribute("class", "divRow");
+  for (let row = 0; row < 16; row++) {
+    let newDivRow = document.createElement('div');
+    newDivRow.setAttribute("class", "divRow");
 
-  for (let col = 0; col < 16; col++) {
-    let box = document.createElement('div');
-    box.setAttribute("class", "box"); 
-    box.textContent = 'text';
-    newDivRow.appendChild(box);
+    for (let col = 0; col < 16; col++) {
+      let box = document.createElement('div');
+      box.setAttribute("class", "box"); 
+      box.textContent = 'text';
+      newDivRow.appendChild(box);
+    }
+    container.appendChild(newDivRow);
   }
-  container.appendChild(newDivRow);
 }
 
+function resetBoard() {
+  let container = document.querySelector('.container');
+  while(container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  createBoard();
+}
+
+createBoard();
+
+let resetButton = document.querySelector('button');
+resetButton.addEventListener('click', resetBoard)
+
+let container = document.querySelector('.container');
 container.addEventListener('mouseover', (event) => {
   var target = event.target;
   if (target.className === 'box' ) {
