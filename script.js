c7825e881c3b4072aed57e8dea9242df0ec3999d
ChooJunIan <ianchoo2000@gmail.com>
2025-03-17 21:37:22 +0800

    Implement user input to adjust sketchpad size

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function createBoard() {
+function createBoard(defaultRowCol = 16) {
   let container = document.querySelector('.container');
 
-  for (let row = 0; row < 16; row++) {
+  for (let row = 0; row < defaultRowCol; row++) {
     let newDivRow = document.createElement('div');
     newDivRow.setAttribute("class", "divRow");
 
-    for (let col = 0; col < 16; col++) {
+    for (let col = 0; col < defaultRowCol; col++) {
       let box = document.createElement('div');
       box.setAttribute("class", "box"); 
-      box.textContent = 'text';
+
       newDivRow.appendChild(box);
     }
     container.appendChild(newDivRow);
@@ -17,10 +17,17 @@ function createBoard() {
 
 function resetBoard() {
   let container = document.querySelector('.container');
+  let newBoardNum = 0;
   while(container.hasChildNodes()) {
     container.removeChild(container.firstChild);
   }
-  createBoard();
+  do {
+    newBoardNum = +prompt("How many rows do you need?") 
+    if (newBoardNum > 100 || newBoardNum < 1) {
+      alert(`You entered ${newBoardNum}. Number cannot be greater than 100 or smaller than 1.`)
+    }
+  } while (newBoardNum > 100 || newBoardNum < 1);
+  createBoard(newBoardNum);
 }
 
 createBoard();
